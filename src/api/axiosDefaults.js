5b53710fd934d7656989af4a2fa9ea4a3e99da7c
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2024-05-22 21:55:17 +0100

    handle auto refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://drf-api-lallenation-0a85182b44cd.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
